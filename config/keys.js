e09842272dac43c9bb5839c222a9da497369e027
Valeria Serci <balerty84@gmail.com>
2019-10-29 22:19:03 +0000

    Set Mongo password as Environment variable

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 const password = process.env.MONGO_PASSWORD
 
 module.exports = {
-    mongoURI: `mongodb+srv://balerty84@gmail.com:${password}@valeser-mzcl3.mongodb.net/test?retryWrites=true&w=majority`
+    mongoURI: `mongodb+srv://Vale:${password}@valeser-mzcl3.mongodb.net/test?retryWrites=true&w=majority`
 }
 
