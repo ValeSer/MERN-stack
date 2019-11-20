fc1f0f3fb66164c0e72781cd528a0366b9b028b4
Valeria Serci <balerty84@gmail.com>
2019-11-20 18:45:55 +0000

    Change database name from test to mern_shopping

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
 const password = process.env.MONGO_PASSWORD
 
 module.exports = {
-    mongoURI: `mongodb+srv://Vale:${password}@valeser-mzcl3.mongodb.net/test?retryWrites=true&w=majority`
+    mongoURI: `mongodb+srv://Vale:${password}@valeser-mzcl3.mongodb.net/mern_shopping?retryWrites=true&w=majority`
 }
-
