dc6fa2906126d4d7e20676fddd12bcbf49e37a11
Valeria Serci <balerty84@gmail.com>
2019-12-11 17:54:29 +0000

    Set GET_ITEMS

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,4 +1,6 @@
 import uuid from 'uuid';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+
 const initialState = {
   items: [
     { id: uuid(), name: 'Eggs'},
@@ -6,4 +8,15 @@ const initialState = {
     { id: uuid(), name: 'Steak'},
     { id: uuid(), name: 'Water'}
   ]
+}
+
+export default function(state = initialState, action) {
+  switch(action.type) {
+    case GET_ITEMS:
+      return {
+        ...state
+      }
+    default:
+      return state;
+  }
 }
